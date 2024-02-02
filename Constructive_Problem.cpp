20694c3f07e71c5911e1080fbd579a345889135f
Mostafa Moin <sa479374@gmail.com>
2024-02-02 18:16:46 +0600

    solution

---
+++ Constructive_Problem.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+
+    // https://codeforces.com/contest/1905/problem/A
+
+    w(x) {
+        int n, m; cin >> n >> m;
+
+        cout << max(n, m) << endl;
+    }
+
+    return 0;
+}
