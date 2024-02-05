b0fe13ad817b58e541f6ba85b79a7ccd65c95b33
Mostafa Moin <sa479374@gmail.com>
2024-02-05 22:10:23 +0600

    bits only

---
+++ Rising_Bacteria1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+
+    // https://codeforces.com/problemset/problem/579/A
+
+    int x; cin >> x;
+
+    cout << setbits(x) << endl;
+
+    return 0;
+}
