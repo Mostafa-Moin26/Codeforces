fda7828a3d20ba029fa8c6d40412a0534580d4c0
Mostafa Moin <sa479374@gmail.com>
2024-02-11 19:16:39 +0600

    solution

---
+++ Make_it_White.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+
+    // https://codeforces.com/contest/1927/problem/A
+
+    w(x) {
+
+        int n; cin >> n;
+        string s; cin >> s;
+
+        cout << s.rfind('B') - s.find('B') + 1 << endl;
+    }
+
+    return 0;
+}
