d5b2721724a85a33bafffa3de0d0acb54cf896ba
Mostafa Moin <sa479374@gmail.com>
2024-02-15 22:49:48 +0600

    solution

---
+++ Sasha_and_the_beautiful_array.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+
+    w(x) {
+        int n; cin >> n;
+
+        vector<ll> a(n);
+
+        for (int i = 0; i < n; ++i) cin >> a[i];
+
+        sort(a.begin(), a.end());
+
+        ll sum = 0;
+
+        for (int i = 1; i < n; ++i) {
+
+            sum += a[i] - a[i - 1];
+        }
+        cout << sum << endl;
+    }
+
+    return 0;
+}
