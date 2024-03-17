ec04f55ce8266192a42d0c9ac58f38694f0ffb84
Mostafa Moin <sa479374@gmail.com>
2024-03-17 10:28:18 +0600

    solution

---
+++ Destroying_Bridge.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int>> vip;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+    // https://codeforces.com/contest/1944/problem/A
+
+    w(x) {
+        int n, k; cin >> n >> k;
+
+        if (k >= n - 1) {
+            cout << 1 << endl;
+        } else {
+            cout << n << endl;
+        }
+    }
+
+    return 0;
+}
