3500c67ee7d1da408fef5a2ef61fde4db282a360
Mostafa Moin <sa479374@gmail.com>
2024-03-29 14:43:09 +0600

    solution

---
+++ Stair_Peak_Neither.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int>> vip;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+    // https://codeforces.com/contest/1950/problem/A
+
+    w(x) {
+        int a, b, c; cin >> a >> b >> c;
+
+        if (a < b  && b < c) {
+            cout << "STAIR" << endl;
+        } else if (b > a && b > c) {
+            cout << "PEAK" << endl;
+        } else {
+            cout << "NONE" << endl;
+        }
+    }
+
+    return 0;
+}
