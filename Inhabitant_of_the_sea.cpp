84b0cfab99dc4d6c6dfa3560765eba7fe6ac7a7b
Mostafa Moin <sa479374@gmail.com>
2024-04-10 09:48:25 +0600

    solution

---
+++ Inhabitant_of_the_sea.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int>> vip;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+    // https://codeforces.com/contest/1955/problem/C
+
+    w(x) {
+        int n, k; cin >> n >> k;
+
+        vi a(n);
+        int sum = 0;
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+            sum += a[i];
+        }
+
+        if (sum <= k) {
+            cout << n << endl;
+            continue;
+        }
+
+        int left = (k + 1) / 2;
+        int right = k / 2;
+        int i = 0;
+        while (left >= a[i]) left -= a[i++];
+        int j = n - 1;
+        while (right >= a[j]) right -= a[j--];
+
+        // cout << i << " " << j << endl;
+        cout << i + n - j - 1 << endl;
+    }
+
+    return 0;
+}
