a28b2ed27c280d30ed98bca6817bcc46639d3c8e
Mostafa Moin <sa479374@gmail.com>
2024-04-13 21:44:10 +0600

    solution

---
+++ Nene_and_the_card_game.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int>> vip;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+    // https://codeforces.com/contest/1956/problem/B
+
+    w(x) {
+        int n; cin >> n;
+
+        set<int> st;
+
+        for (int i = 0; i < n; ++i) {
+            int num; cin >> num;
+
+            st.insert(num);
+        }
+
+        cout << n - st.size() << endl;
+    }
+
+    return 0;
+}
