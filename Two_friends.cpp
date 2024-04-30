f148c4c09c0b7bf7800d5289b0ca0a5b3f1701be
Mostafa Moin <sa479374@gmail.com>
2024-04-30 19:44:19 +0600

    solution

---
+++ Two_friends.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int>> vip;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+    // https://codeforces.com/contest/1969/problem/A
+
+    w(x) {
+
+        int n; cin >> n;
+
+        int arr[n + 1];
+
+        for (int i = 1; i <= n;  ++i) cin >> arr[i];
+
+        bool f = false;
+
+        for (int i = 1; i <= n; ++i) {
+
+            if (arr[arr[i]] == i) {
+                f = true;
+                break;
+            }
+        }
+
+        if (f) {
+            cout << 2 << endl;
+        } else {
+            cout << 3 << endl;
+        }
+    }
+    return 0;
+}
