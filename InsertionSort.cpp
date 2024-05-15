9df87c134bda122f47c40afac73f38ff432b00d8
Mostafa Moin <sa479374@gmail.com>
2024-05-15 09:32:44 +0600

    insertion Sort code

---
+++ InsertionSort.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+
+      for (int k = 0; k < n; ++k) cout << arr[k] << " ";
+
+    cout << endl;
+    }
+
+}
+
+int main()
+{
+   int n; cin >> n;
+
+   int arr[n];
+
+   for (int i = 0; i < n; ++i) cin >> arr[i];
+
+    insertionSort(arr, n);
+
+    return 0;
+}
+
