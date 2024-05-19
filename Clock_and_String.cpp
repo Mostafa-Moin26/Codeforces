89eb115b87ae5afbe257c02b07820ada8cf247f5
Mostafa Moin <sa479374@gmail.com>
2024-05-19 23:50:59 +0600

    some line removed

--- Clock_and_String.cpp
+++ Clock_and_String.cpp
@@ -13,10 +13,6 @@ typedef vector<pair<int, int>> vip;
 #define ff              first
 #define ss              second
 #define mp              make_pair
-#define REP(i,a,b) for (int i = a; i <= b; i++)
-#define ps(x,y)         fixed<<setprecision(y)<<x
-#define setbits(x)      __builtin_popcountll(x)
-#define zrobits(x)      __builtin_ctzll(x)
 
 int main() {
 
