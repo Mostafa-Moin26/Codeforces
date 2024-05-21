c9f2640a2b2e61e43e847a9168c179763cea3f49
Mostafa Moin <sa479374@gmail.com>
2024-05-21 07:09:24 +0600

    using formula

---
+++ Phone_Desktop_optimise.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int>> vip;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+    https://codeforces.com/contest/1974/problem/A
+
+    w(x) {
+      int x, y; cin >> x >> y;
+
+      cout << max((y + 1) / 2, (x + y * 4 + 14) / 15) << endl;
+
+    }
+    return 0;
+}
