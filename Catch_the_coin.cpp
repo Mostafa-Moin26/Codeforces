4196e8fe983e5b7d488a7d6483915274dcfc5643
Mostafa Al Moin <sa479374@gmail.com>
2024-07-07 14:58:02 +0600

    solution

---
+++ Catch_the_coin.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// https://codeforces.com/contest/1989/problem/A
+
+int main() {
+
+  int t; cin >> t;
+
+  while (t--) {
+
+    int x, y; cin >> x >> y;
+
+    if (y < -1) {
+      cout << "NO" << endl;
+    } else {
+      cout << "YES" << endl;
+    }
+  }
+
+  return 0;
+}
