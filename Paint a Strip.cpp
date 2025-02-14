2688ba213269485beadc78cd3b696e7e807efbd4
Mostafa Al Moin <sa479374@gmail.com>
2025-02-14 10:16:21 +0600

    Paint a Strip

---
+++ Paint a Strip.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int>> vip;
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define int              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define all(x) (x).begin(), (x).end()
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+int gcd(int a, int b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+bool comp(int a, int b) {return a > b;}
+
+signed main() {
+    Mostafa
+
+    // https://codeforces.com/problemset/problem/2040/B
+
+    w(x) {
+        int n; cin >> n;
+
+        int x = 1, cnt = 0;
+
+        while (x < n) {
+            x = x * 2 + 2;
+            cnt++;
+        }
+
+        cout << cnt + 1 << endl;
+    }
+    return 0;
+}
