85a98b874d022bfdb2eaf9a642cdd45bf60d8266
Mostafa Al Moin <sa479374@gmail.com>
2025-02-15 12:45:23 +0600

    The Strict Teacher (Easy Version)

---
+++ The Strict Teacher (Easy Version).cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int>> vip;
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define int              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define all(x) (x).begin(), (x).end()
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+int gcd(int a, int b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+bool comp(int a, int b) {return a > b;}
+signed main() {
+    Mostafa
+
+    // https://codeforces.com/problemset/problem/2005/B1
+    
+    w(x) {
+        int n, m, q; cin >> n >> m >> q;
+        int b1, b2, d;
+
+        cin >> b1 >> b2 >> d;
+        if (b1 > b2) swap(b1, b2);
+
+        if (d > b2) {
+            cout << n - b2 << endl;
+        } else if (d < b1) {
+            cout << b1 - 1 << endl;
+        } else {
+            cout << (b2 - b1) / 2 << endl;
+        }
+    }
+    return 0;
+}
