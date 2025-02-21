9867aa1b7c852ca03bd612f8b51605b23f94584d
Mostafa Al Moin <sa479374@gmail.com>
2025-02-21 08:20:03 +0600

    Subtract Min Sort

---
+++ Subtract Min Sort.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int>> vip;
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define int              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define all(x) (x).begin(), (x).end()
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+int gcd(int a, int b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+bool comp(int a, int b) {return a > b;}
+signed main() {
+    Mostafa
+
+    // https://codeforces.com/problemset/problem/2060/D
+
+    w(x) {
+        int n; cin >> n;
+        vi a(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        bool f = true;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            if (a[i] <= a[i + 1]) {
+                a[i + 1] -= a[i];
+            } else {
+                f = false;
+                break;
+            }
+        }
+
+        if (f) yes;
+        else no;
+    }
+    return 0;
+}
