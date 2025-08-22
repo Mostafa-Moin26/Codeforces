b5157364fec8c153ca8d0d54238caa2fd4d49eb7
Mostafa Al Moin <sa479374@gmail.com>
2025-08-22 08:37:58 +0600

    Homework

---
+++ Homework.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define w(x)            int x;cin>>x;while(x--)
+#define int              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+signed main() {
+    Mostafa
+
+    // https://codeforces.com/contest/2132/problem/A
+
+    w(x) {
+        int n, m;
+        string a, b, c;
+
+        cin >> n >> a >> m >> b >> c;
+
+        for (int i = 0; i < c.size(); i++) {
+
+            if (c[i] == 'D') {
+                a.push_back(b[i]);
+            } else {
+                a.insert(a.begin(), b[i]);
+            }
+        }
+
+        cout << a << endl;
+    }
+    return 0;
+}
