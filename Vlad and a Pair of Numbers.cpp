05edce76bb4da22d55269821a71624ee69000755
Mostafa Al Moin <sa479374@gmail.com>
2025-12-06 13:55:54 +0600

    Vlad and a Pair of Numbers

---
+++ Vlad and a Pair of Numbers.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define w(x)            int x;cin>>x;while(x--)
+#define int              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+signed main() {
+    Mostafa
+
+    w(t) {
+        int n; cin >> n;
+
+        if(n % 2) {
+            cout << -1 << '\n';
+
+        } else {
+
+            int a = 2 * n - (n / 2);
+            int b = n / 2;
+
+            if((a ^ b) == n) cout << b <<" "<< a <<"\n";
+
+            else cout << -1 << "\n";
+        }
+    }
+    return 0;
+}
