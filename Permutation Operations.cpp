fb414eb7bc99cf1720b9461f3e1d619214435524
Mostafa Al Moin <sa479374@gmail.com>
2026-01-26 11:19:50 +0600

    Permutation Operations

---
+++ Permutation Operations.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define Mostafa ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define w(x) int x;cin>>x;while(x--)
+#define int long long
+#define pb push_back
+#define ff first
+#define ss second
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+
+signed main() {
+    Mostafa
+
+    w(t) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for(int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        vector<pair<int,int>> p;
+
+        for(int i = 0; i < n - 1; i++) {
+            p.pb({a[i] - a[i + 1], i});
+        }
+
+        sort(p.begin(), p.end());
+
+        cout << 1;
+
+        for(auto &pp : p) {
+            cout << " " << pp.ss + 2;
+        }
+
+        cout << "\n";
+    }
+    
+    return 0;
+}
