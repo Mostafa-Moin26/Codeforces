0ddb1bb6b32d61f2d79eb009993a10d1b890b8c0
Mostafa Al Moin <sa479374@gmail.com>
2026-01-30 21:05:25 +0600

    Row Major

---
+++ Row Major.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define Mostafa ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define w(x) int x;cin>>x;while(x--)
+#define int long long
+#define pb push_back
+#define ff first
+#define ss second
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+
+
+signed main() {
+    Mostafa
+
+    w(t) {
+        int n;
+        cin >> n;
+
+        int c = 1;
+
+        while(n % c == 0) {
+            c++;
+        }
+
+        for(int i = 0; i < n; i++) {
+            cout << (char)('a' + i % c);
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
