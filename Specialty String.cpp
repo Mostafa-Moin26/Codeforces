0b4672cd02ebae8bc3c18b3230f67930c7be0f5e
Mostafa Al Moin <sa479374@gmail.com>
2026-02-28 09:30:53 +0600

    Specialty String

---
+++ Specialty String.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define w(x)            int x;cin>>x;while(x--)
+#define int              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+signed main() {
+    Mostafa 
+
+    // https://codeforces.com/contest/2200/problem/C
+
+    w(t) {
+        int n;  
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        vector<char> a; 
+
+        for (char ch : s) {
+            if (!a.empty() && a.back() == ch) {
+                a.pop_back();
+            } else {
+                a.pb(ch);
+            }
+        }
+
+        if (a.empty()) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+    return 0;
+}
